eb9fa38255c7d4f916829bfa01f31b061bba1bb5
Guilherme Pereira <guilherme@affogata.com>
2022-12-29 16:36:41 +0000

    Add a starting point

--- lib/betteruptime/job_scheduler.rb
+++ lib/betteruptime/job_scheduler.rb
@@ -7,7 +7,7 @@ module Betteruptime
         Betteruptime.configuration.jobs.each do |c|
           Sidekiq.set_schedule("heartbeat_#{c.name.to_s.downcase}",
             { 
-              cron: '0 */5 * * * *',
+              every: ['5m', first_in: '5m'],
               class: 'Betteruptime::GenericJob',
               args: [c.name, c.block.to_source]
             }
